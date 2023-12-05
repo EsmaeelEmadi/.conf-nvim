be4dff726f0a822b9205a8a3fa98774192a61afb
Naeim <naeim.fa@gmail.com>
2023-12-05 16:35:02 +0330

    autoformat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -209,9 +209,9 @@ local async = event == "BufWritePost"
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
+      -- vim.keymap.set("n", "<Leader>f", function()
+      --   vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      -- end, { buffer = bufnr, desc = "[lsp] format" })
 
       -- format on save
       vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
